9977caa72abf29f4d10da9c78fc9152c276f3baa
Gaurav Sunthwal <gauravsunthwal@glimznow.com>
2026-04-06 09:05:02 +0530

    chore: remove unused test-imports.js file

--- attendance-app/test-imports.js
+++
@@ -1,8 +0,0 @@
-
-const faceapi = require('face-api.js');
-const tf = require('@tensorflow/tfjs-node');
-const { Canvas, Image, ImageData } = require('canvas');
-
-console.log('Imports successful');
-faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
-console.log('MonkeyPatch successful');
